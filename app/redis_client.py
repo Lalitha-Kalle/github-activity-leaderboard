d41efaedb13f4d962c843d0fdd3ff340295e3299
Lalitha-Kalle <lalithaprogramming@gmail.com>
2026-05-11 21:02:46 +0530

    regenerated clean requirements file

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,7 +1,7 @@
 import redis.asyncio as redis
 from config import settings
 
-redis_client = redis.from_url(settings.REDIS_URL, decode_responses=True)
+redis_client = redis.from_url(settings.REDIS_URL, decode_responses=True, ssl_cert_reqs=None)
 
 async def get_redis():
     return redis_client
